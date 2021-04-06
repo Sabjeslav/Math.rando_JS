7422634ad82b07d7f5a2e52442a87414dfa02056
s4tenko <s4tenko@gmail.com>
2021-04-06 09:29:09 +0300

    User contructor added

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+
 const test = new Array();
 for (let i = 0; i < 10; i++) {
   test.push(getRandomInt(100, 200));
@@ -16,14 +17,22 @@ for (let i = 0; i < 10; i++) {
   Поле age заполняется рандомно от 12 до 50 лет. (Только целые числа).
   Поле isMale заполняется рандомно значениями true или false. */
 
+const User = function (email, age, isMale) {
+  this.email = email;
+  this.age = age;
+  this.isMale = isMale;
+}
+  
 function createObjects(amount) {
   const userArray = [];
   for (let i = 0; i < amount; i++) {
-    userArray[i] = {
-      email: `user${i}@gmail.com`,
-      age: getRandomInt(12, 50),
-      isMale: Boolean(Math.random() < 0.5)
-    } 
+    const email = `user${i}@gmail.com`;
+    const age = getRandomInt(12, 50);
+    const isMale = Boolean(Math.random() < 0.5);
+    const user = new User(email, age, isMale);
+    userArray.push(user)
   }
   return userArray;
 }
+
+console.log(createObjects(5));
